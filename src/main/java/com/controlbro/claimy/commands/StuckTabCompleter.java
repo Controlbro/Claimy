6cbe17614e4f2c8ed3e4abc155c57a294b1951ad
Controlbro <54811794+Controlbro@users.noreply.github.com>
2026-01-04 11:54:07 -0500

    Block mob and villager damage in claims

---
+++ src/main/java/com/controlbro/claimy/commands/StuckTabCompleter.java
@@ -0,0 +1,15 @@
+package com.controlbro.claimy.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Collections;
+import java.util.List;
+
+public class StuckTabCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return Collections.emptyList();
+    }
+}
